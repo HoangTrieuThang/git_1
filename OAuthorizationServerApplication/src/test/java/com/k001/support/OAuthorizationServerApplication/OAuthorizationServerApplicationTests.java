f7755ba46ef56d9159581634622f727f8e651057
HoangTrieuThang <valentine.tnhn@gmail.com>
2019-11-16 17:11:56 +0700

    add server

---
+++ OAuthorizationServerApplication/src/test/java/com/k001/support/OAuthorizationServerApplication/OAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k001.support.OAuthorizationServerApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
